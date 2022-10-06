10b42918f1ec481c0b1d4ac73efb78ea69e6012f
AliAhmedKhan <alistpaulscr7@gmail.com>
2022-10-06 20:22:56 +0500

    Added Redirects

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/mainpage" element={<Main />} />
       </Routes>
